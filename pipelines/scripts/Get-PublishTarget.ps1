3a62cd4442fc99b2446598cc09a50b0a9d4a11ce
Sunny Bharne <sunny.bharne.automation@gmail.com>
2024-06-22 15:02:04 +0300

    testimg

--- pipelines/scripts/Get-PublishTarget.ps1
+++ pipelines/scripts/Get-PublishTarget.ps1
@@ -5,12 +5,10 @@ param (
     [Parameter(Mandatory =$true, Position = 1, HelpMessage = "Container registry")]
     [ValidateNotNullOrEmpty()]
     [string]$acr
-
     [Parameter(Mandatory =$true, Position = 0, HelpMessage = "Module file location")]
     [ValidateNotNullOrEmpty()]
     [ValidateSet("modules/resources/*.bicep", "modules/services/*.bicep", "modules/products/*.bicep")]
     [string]$file
-
     [Parameter(Mandatory =$true, Position = 2, HelpMessage = "Module version")]
     [ValidateNotNullOrEmpty()]
     [string]$version
