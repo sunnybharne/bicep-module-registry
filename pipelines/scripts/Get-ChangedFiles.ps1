36234b1296c6d71579fd3795a146156a3424f3bd
Sunny Bharne <sunny.bharne.automation@gmail.com>
2024-06-22 22:06:47 +0300

    testing

--- pipelines/scripts/Get-ChangedFiles.ps1
+++ pipelines/scripts/Get-ChangedFiles.ps1
@@ -42,7 +42,7 @@ if ($diffOutput) {
 # Split the diff output into an array of file paths
 $changedFiles = $diffOutput -split "`n"
 
-Write-Output changedFiles.Length
+Write-Output $changedFiles.Length
 
 # Return changedFiles
 return $changedFiles
